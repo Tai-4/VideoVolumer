1a82b85fd8bf9d46c27973af3ce7cc98605cea40
Tai-4 <ka1ntrk@gmail.com>
2022-02-28 02:28:59 +0900

    popupの仮組み

--- Popup/popup.js
+++ Popup/popup.js
@@ -1,15 +1,45 @@
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-      chrome.declarativeContent.onPageChanged.addRules([{
-        conditions: [
-          new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: {hostEquals: 'drive.google.com', schemes: ['https'] },
-          })
-        ],
-        actions: [
-          new chrome.declarativeContent.ShowPageAction()
-        ]
-      }]);
-    });
-  });
-  
+class Input{
+    static{
+        this.volumeLevel = {
+            element: document.getElementById("volumeLevelInput"),
+            getValue: function(){ return; }
+        };
+    }
+}
+
+class Display{
+    static{
+        this.pagefavicon = {
+            element: document.getElementById("pagefaviconDisplay"),
+            set = function(value){ element.src = value; }
+        };
+        this.pageTitle = {
+            element: document.getElementById("pageTitleDisplay"),
+            set = function(value){ element.innerText = value; }
+        };
+        this.volumeLevel ={
+            element: document.getElementById("volumeLevelDisplay"),
+            set = function(value){ content.innerText = value + "%"; }
+        };
+    }
+}
+
+main();
+
+function main(){
+    initializeUI();
+}
+
+function initializeUI(){
+    const currentTab = await getCurrentTab();
+    Display.pagefavicon.set();
+    Display.pageTitle.set(currentTab.title);
+    Display.volumeLevel.set(100);
+}
+
+async function getCurrentTab(){
+    const queryOptions = { active: true, currentWindow: true };
+    const tabList = await chrome.tabs.query(queryOptions);
+    const currentTab = tabList[0];
+    return currentTab;
+}
